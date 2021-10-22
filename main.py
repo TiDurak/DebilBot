5bc3447fdde6eee5d311419298b3bb16b0de99ca
TiDurak <bsodrussb@gmail.com>
2021-10-22 10:08:18 +0300

    OOP!!!

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from config import settings
 import converters
 import games
 import help_command
@@ -6,14 +5,12 @@ import moderation
 import music_bot
 import listeners
 import text_commands
+from config import settings
 
-### Discord Libs ###
 import discord
 from discord.ext import commands
 from discord.ext.commands import has_permissions, CommandNotFound, MissingPermissions, CommandInvokeError, MemberNotFound, BotMissingPermissions
-
 from discord_components import DiscordComponents
-### Discord Libs ###
 
 
 bot = commands.Bot(command_prefix = settings['prefix'])
@@ -39,17 +36,5 @@ listeners.setup(bot)
 
 text = text_commands.Text(bot)
 text_commands.setup(bot)
-
-### Errors catching ###
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, CommandNotFound):
-        await ctx.send('❌ Комманда не существует!')
-    elif isinstance(error, MissingPermissions):
-        await ctx.send('❌ У вас нету привилегий управления сообщениями!')
-    elif isinstance(error, MemberNotFound):
-        await ctx.send('❌ Участник не найден!')
-### Errors catching ###
-
 
 bot.run(settings['token'])
