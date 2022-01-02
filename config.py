4e6da033087359e6cd17dcbfc8864bb8a952a356
TiDurak <bsodrussb@hmail.com>
2022-01-02 17:33:51 +0200

    Config update

--- config.py
+++ config.py
@@ -6,8 +6,13 @@ except KeyError:
     print('Config variable "DEBIL_TOKEN" is unreachable. Please, add this!')
     exit()
 
+path = {
+    'windows': r'C:\Program Files\ffmpeg\bin\ffmpeg.exe',
+    'linux': 'ffmpeg'
+}
+
 settings = {
     'token': os.environ['DEBIL_TOKEN'],
     'prefix': 'd.',
-    'path_to_ffmpeg': r'C:\Program Files\ffmpeg\bin\ffmpeg.exe', # Path to ffmpeg. For Linux users just type "ffmpeg"
+    'path_to_ffmpeg': path.get('linux') # If u use Windows, type path.get('windows'), else if u use Linux, or Heroku, type path.get('linux')
 }
