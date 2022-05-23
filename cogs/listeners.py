71bc2d190988d111169827e449d29baff776dbf7
TiDurak <bsodrussb@hmail.com>
2022-05-23 15:19:37 +0200

    Rewriting bot user activities

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -28,22 +28,14 @@ class OnReady(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print('[b green] Bot is ready! Just type d.help to see all bot commands.')
+        activities = [discord.Activity(type=discord.ActivityType.watching, name="зомбоящик"),
+                      discord.Activity(type=discord.ActivityType.watching, name="на писюны"),
+                      discord.Activity(type=discord.ActivityType.listening, name="ровный базар"),
+                      discord.Activity(type=discord.ActivityType.watching, name="порнушку"),
+                      discord.Game(name="игру")]
         while True:
-            random_int = random.randint(0, 4)
-
-            match random_int:
-                case 0:
-                    await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="ровный базар"))
-                case 1:
-                    await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="порнушку"))
-                case 2:
-                    await self.bot.change_presence(activity=discord.Game(name="игру"))
-                case 3:
-                    await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="зомбоящик"))
-                case 4:
-                    await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="на писюны"))
-
-
+            activity = activities[random.randint(0, 4)]
+            await self.bot.change_presence(status=discord.Status.online, activity=activity)
             await asyncio.sleep(5)
             await self.bot.change_presence(status=discord.Status.online, activity=discord.Game(f"{settings.get('prefix')}help"))
             await asyncio.sleep(5)
