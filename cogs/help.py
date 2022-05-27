ce6a08a73e9a0d9e38fbe910025d5ca96e42c732
TiDurak <bsodrussb@hmail.com>
2022-05-27 09:28:57 +0200

    Help command bug fix

--- cogs/help.py
+++ cogs/help.py
@@ -166,72 +166,59 @@ class Help(commands.Cog):
         embed = discord.Embed(color = 0xffcd4c , title = 'poll', description = helptext)
         await ctx.send(
         embed = embed,
-        components = [[Button(style = ButtonStyle.blue, label = 'Результат Плохого Примера')]]
-        )
-
-        responce = await self.bot.wait_for('button_click', check = lambda message: message.author == ctx.author)
-        if responce.component.label == 'Результат Плохого Примера':
-            Desc = ('1⃣ чипсы \n'
-                    '2⃣ вы\n'
-                    '3⃣ предпочитаете\n'
-                    '4⃣ Lais \n'
-                    '5⃣ Prongls\n'
-                    '6⃣ 2\n'
-                    '7⃣ корочки')
-            BadExample = discord.Embed(color = 0xffcd4c , title = f'{self.bot.get_emoji(settings["emojis"]["stonks"])} GamerDisclaimer#7647: Какие', description=Desc)
-            await responce.respond(embed=BadExample)
+        components = [[Button(style = ButtonStyle.blue, label = 'Результат Плохого Примера')]])
                 
-        @help.command()
-        async def echo(self, ctx):
-            helptext = ('```d.echo <текст>```\n'
-                        'Повторяет сообщение за вами')
-            embed = discord.Embed(color = 0xffcd4c , title = 'echo', description = helptext)
+    @help.command()
+    async def echo(self, ctx):
+        helptext = ('```d.echo <текст>```\n'
+                    'Повторяет сообщение за вами')
+        embed = discord.Embed(color = 0xffcd4c , title = 'echo', description = helptext)
+        await ctx.send(embed = embed)
+
+    @help.command()
+    async def enchode_b64(self, ctx):
+        helptext = ('```d.encode_b64 <текст>```\n'
+                    'Конвертирует текст в Base64')
+        embed = discord.Embed(color = 0xffcd4c , title = 'encode_b64', description = helptext)
+        await ctx.send(embed = embed)
+
+    @help.command()
+    async def decode_b64(self, ctx):
+            helptext = ('```d.decode_b64 <base64 текст>```\n'
+                        'Конвертирует ваш Base64 код в ноормальный, понятный любому человеку (кроме фаната а4) текст')
+            embed = discord.Embed(color = 0xffcd4c , title = 'decode_b64', description = helptext)
             await ctx.send(embed = embed)
 
-        @help.command()
-        async def enchode_b64(self, ctx):
-            helptext = ('```d.encode_b64 <текст>```\n'
-                        'Конвертирует текст в Base64')
-            embed = discord.Embed(color = 0xffcd4c , title = 'encode_b64', description = helptext)
-            await ctx.send(embed = embed)
+    @help.command()
+    async def encode_binary(self, ctx):
+        helptext = ('```d.encode_binary <текст>```\n'
+                    'Конвертирует текст в бинарный код')
+        embed = discord.Embed(color = 0xffcd4c , title = 'encode_binary', description = helptext)
+        await ctx.send(embed = embed)
 
-        @help.command()
-        async def decode_b64(self, ctx):
-                helptext = ('```d.decode_b64 <base64 текст>```\n'
-                            'Конвертирует ваш Base64 код в ноормальный, понятный любому человеку (кроме фаната а4) текст')
-                embed = discord.Embed(color = 0xffcd4c , title = 'decode_b64', description = helptext)
-                await ctx.send(embed = embed)
+    @help.command()
+    async def decode_binary(self, ctx):
+        helptext = ('```d.decode_binary <бинарный код>```\n'
+                    'Конвертирует бинарный код в текст')
+        embed = discord.Embed(color = 0xffcd4c , title = 'decode_binary', description = helptext)
+        await ctx.send(embed = embed)
 
-        @help.command()
-        async def encode_binary(self, ctx):
-            helptext = ('```d.encode_binary <текст>```\n'
-                        'Конвертирует текст в бинарный код')
-            embed = discord.Embed(color = 0xffcd4c , title = 'encode_binary', description = helptext)
-            await ctx.send(embed = embed)
+    @help.command()
+    async def slots(self, ctx):
+        helptext = ('```d.slots```\n'
+                    'Игра в однорукого бандита (бесплатно, без регистрации и смс)')
+        embed = discord.Embed(color = 0xffcd4c , title = 'slots', description = helptext)
+        await ctx.send(embed = embed)
 
-        @help.command()
-        async def decode_binary(self, ctx):
-            helptext = ('```d.decode_binary <бинарный код>```\n'
-                        'Конвертирует бинарный код в текст')
-            embed = discord.Embed(color = 0xffcd4c , title = 'decode_binary', description = helptext)
-            await ctx.send(embed = embed)
-
-        @help.command()
-        async def slots(self, ctx):
-            helptext = ('```d.slots```\n'
-                        'Игра в однорукого бандита (бесплатно, без регистрации и смс)')
-            embed = discord.Embed(color = 0xffcd4c , title = 'slots', description = helptext)
-            await ctx.send(embed = embed)
-
-        @help.command()
-        async def janken(self, ctx):
-            helptext = ('```d.janken```\n'
-                        'Классические камень-ножницы-бумага.\n'
-                        'Правила обьяснять не буду, ибо их итак все знают\n'
-                        '(Кстать, *это первая команда с алиасами!*)\n'
-                        'Алиасы: `d.rockpaperscissors`, `d.rps`\n')
-            embed = discord.Embed(color = 0xffcd4c , title = 'Камень-Ножницы-Бумага', description = helptext)
-            await ctx.send(embed = embed)
+    @help.command()
+    async def janken(self, ctx):
+        helptext = ('```d.janken```\n'
+                    'Классические камень-ножницы-бумага.\n'
+                    'Правила обьяснять не буду, ибо их итак все знают\n'
+                    '(Кстать, *это первая команда с алиасами!*)\n'
+                    'Алиасы: `d.rockpaperscissors`, `d.rps`\n')
+        embed = discord.Embed(color = 0xffcd4c , title = 'Камень-Ножницы-Бумага', description = helptext)
+        await ctx.send(embed = embed)
 
 def setup(bot):
         bot.add_cog(Help(bot))
