b4055704c0b00ead8546bd15058ec2b572e0f4d7
TiDurak <ivanplisko9@gmail.com>
2023-06-17 20:15:39 +0200

    Removed some-random-api.com API dependencies

--- cogs/converters.py
+++ cogs/converters.py
@@ -1,8 +1,7 @@
+import base64
+
 import discord
 from discord.ext import commands
-
-import json
-import requests
 
 
 class Convert(commands.Cog):
@@ -13,40 +12,40 @@ class Convert(commands.Cog):
     async def encode_b64(self, ctx, *, message):
         """Кодирует твоё сообщение в формате BASE64"""
 
-        response = requests.get(f'https://some-random-api.com/base64?encode={message}')
-        json_data = json.loads(response.text)
+        text_bytes = message.encode('utf-8')  # Кодируем текст в байтовую последовательность
+        base64_bytes = base64.b64encode(text_bytes)  # Кодируем байты в base64
+        base64_text = base64_bytes.decode('utf-8')
 
-        embed = discord.Embed(color=0xff8080, title='Base64 Encoder', description=json_data['base64'])
+        embed = discord.Embed(color=0xff8080, title='Base64 Encoder', description=base64_text)
         await ctx.send(embed=embed)
 
     @commands.command()
     async def decode_b64(self, ctx, *, message):
         """Декодирует BASE64 текст в нормальный, охуенный текст"""
 
-        response = requests.get(f'https://some-random-api.com/base64?decode={message}')
-        json_data = json.loads(response.text)
+        base64_bytes = message.encode('utf-8')  # Кодируем base64 текст в байтовую последовательность
+        text_bytes = base64.b64decode(base64_bytes)  # Декодируем base64 байты обратно в исходные байты
+        decoded_text = text_bytes.decode('utf-8')
 
-        embed = discord.Embed(color=0xff8080, title='Base64 Decoder', description=json_data['text'])
+        embed = discord.Embed(color=0xff8080, title='Base64 Decoder', description=decoded_text)
         await ctx.send(embed=embed)
 
     @commands.command()
     async def encode_binary(self, ctx, *, message):
         """Кодирует твоё сообщение в бинарный код"""
 
-        response = requests.get(f'https://some-random-api.com/binary?encode={message}')
-        json_data = json.loads(response.text)
+        binary_code = ' '.join(format(ord(char), '08b') for char in message)
 
-        embed = discord.Embed(color=0xff8080, title='Binary Encoder', description=json_data['binary'])
+        embed = discord.Embed(color=0xff8080, title='Binary Encoder', description=binary_code)
         await ctx.send(embed=embed)
 
     @commands.command()
     async def decode_binary(self, ctx, *, message):
         """Декодирует бинарный код в нормальный, охуенный текст"""
 
-        response = requests.get(f'https://some-random-api.com/binary?decode={message}')
-        json_data = json.loads(response.text)
+        text = ''.join(chr(int(binary, 2)) for binary in message.split())
 
-        embed = discord.Embed(color=0xff8080, title='Binary Decoder', description=json_data['text'])
+        embed = discord.Embed(color=0xff8080, title='Binary Decoder', description=text)
         await ctx.send(embed=embed)
 
 
