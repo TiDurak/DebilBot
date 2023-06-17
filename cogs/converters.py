8fa50b360ff2e62bab08f08f9b4da6fe8e81c3a3
TiDurak <ivanplisko9@gmail.com>
2023-06-17 15:45:42 +0200

    Fixed all encode-decode commands

--- cogs/converters.py
+++ cogs/converters.py
@@ -13,7 +13,7 @@ class Convert(commands.Cog):
     async def encode_b64(self, ctx, *, message):
         """Кодирует твоё сообщение в формате BASE64"""
 
-        response = requests.get(f'https://some-random-api.ml/base64?encode={message}')
+        response = requests.get(f'https://some-random-api.com/base64?encode={message}')
         json_data = json.loads(response.text)
 
         embed = discord.Embed(color=0xff8080, title='Base64 Encoder', description=json_data['base64'])
@@ -23,7 +23,7 @@ class Convert(commands.Cog):
     async def decode_b64(self, ctx, *, message):
         """Декодирует BASE64 текст в нормальный, охуенный текст"""
 
-        response = requests.get(f'https://some-random-api.ml/base64?decode={message}')
+        response = requests.get(f'https://some-random-api.com/base64?decode={message}')
         json_data = json.loads(response.text)
 
         embed = discord.Embed(color=0xff8080, title='Base64 Decoder', description=json_data['text'])
@@ -33,7 +33,7 @@ class Convert(commands.Cog):
     async def encode_binary(self, ctx, *, message):
         """Кодирует твоё сообщение в бинарный код"""
 
-        response = requests.get(f'https://some-random-api.ml/binary?encode={message}')
+        response = requests.get(f'https://some-random-api.com/binary?encode={message}')
         json_data = json.loads(response.text)
 
         embed = discord.Embed(color=0xff8080, title='Binary Encoder', description=json_data['binary'])
@@ -43,7 +43,7 @@ class Convert(commands.Cog):
     async def decode_binary(self, ctx, *, message):
         """Декодирует бинарный код в нормальный, охуенный текст"""
 
-        response = requests.get(f'https://some-random-api.ml/binary?decode={message}')
+        response = requests.get(f'https://some-random-api.com/binary?decode={message}')
         json_data = json.loads(response.text)
 
         embed = discord.Embed(color=0xff8080, title='Binary Decoder', description=json_data['text'])
