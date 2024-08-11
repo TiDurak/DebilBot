395c19a1a7e99ba7a7eb867e2f10ec07bef40857
TiDurak <ivanplisko9@gmail.com>
2024-08-11 14:25:40 +0200

    idk, just a commit. nobody reads this text. lorem ipsum. i <3 ur mom

--- main.py
+++ main.py
@@ -18,21 +18,15 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix=settings['prefix'], intents=intents)
 
-asyncio.run(converters.setup(bot))
-asyncio.run(fun.setup(bot))
-asyncio.run(help.setup(bot))
-asyncio.run(moderation.setup(bot))
-asyncio.run(information.setup(bot))
-asyncio.run(music.setup(bot))
-asyncio.run(listeners.setup(bot))
-asyncio.run(text.setup(bot))
-asyncio.run(c_fun.setup(bot))
-asyncio.run(c_information.setup(bot))
-asyncio.run(s_fun.setup(bot))
-asyncio.run(s_text.setup(bot))
-asyncio.run(s_music.setup(bot))
-asyncio.run(s_moderation.setup(bot))
-asyncio.run(s_converters.setup(bot))
+basic_cogs = [converters.setup(bot), fun.setup(bot), help.setup(bot), moderation.setup(bot),
+              information.setup(bot), music.setup(bot), listeners.setup(bot), text.setup(bot)]
+slash_cogs = [s_fun.setup(bot), s_text.setup(bot), s_music.setup(bot), s_moderation.setup(bot),
+              s_converters.setup(bot)]
+context_menu_cogs = [c_fun.setup(bot), c_information.setup(bot)]
+cogs_array = [basic_cogs, slash_cogs, context_menu_cogs]
+for array in cogs_array:
+    for cog in array:
+        asyncio.run(cog)
 
 print("[b i blue]Starting a bot. It may take a few seconds")
 
